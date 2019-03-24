4b158b9bfe47bab8adc0d1b3d106eb857ea84940
Jacob Urmanec <jurmanec@gmail.com>
2019-03-23 21:30:15 -0400

    Now I am committing.

---
+++ com.jake/HotBevException.java
@@ -0,0 +1,8 @@
+public class HotBevException extends Exception {
+	//constructors
+	public HotBevException() {
+	}
+	public HotBevException(String arg0) {
+		super(arg0); 
+	}
+}
---
+++ com.jake/NotHotBevException.java
@@ -0,0 +1,8 @@
+public class NotHotBevException extends Exception {
+	//constructors
+	public NotHotBevException() {
+	}
+	public NotHotBevException(String arg0) {
+		super(arg0);
+	}
+}
